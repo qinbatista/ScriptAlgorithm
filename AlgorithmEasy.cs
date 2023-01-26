996647ea26f2314c5fdbe7d4210f2504a5fe558f
qin.batista <qin.batista@outlook.com>
2023-01-26 01:04:11 -0800

    save change

---
+++ AlgorithmEasy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    class AlgorithmEasy
+    {
+        //create a dictionary to store the value and index
+        //if the value is not in the diction, add how many remain value is needed to get the target
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> _value_dic = new Dictionary<int, int>();
+            int _remain = 0;
+            int[] returnValue = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _remain = target - nums[i];
+                if (_value_dic.ContainsKey(_remain))
+                {
+                    returnValue[0] = _value_dic[_remain];
+                    returnValue[1] = i;
+                    return returnValue;
+                }
+                else
+                {
+                    if (!_value_dic.ContainsKey(nums[i]))
+                        _value_dic.Add(nums[i], value: i);
+                }
+            }
+            return returnValue;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,37 +1,16 @@
 ﻿using System;
+
 namespace Algorithm
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine(value: "Hello World!");
-			int[] nums = new int[] { 2, 11, 11, 7 };
-			int target = 9;
-			var program = new Program();
-			Console.WriteLine(string.Join(",",program.TwoSum(nums,target)));
-		}
-		public int[] TwoSum(int[] nums,int target)
-		{
-			Dictionary<int, int> _value_dic = new Dictionary<int, int>();
-			int _remain = 0;
-			int[] returnValue = new int[2];
-			for (int i = 0; i < nums.Length; i++)
-			{
-				_remain = target - nums[i];
-				if (_value_dic.ContainsKey(_remain))
-				{
-					returnValue[0] = _value_dic[_remain];
-					returnValue[1] = i;
-					return returnValue;
-				}
-				else
-				{
-					if (!_value_dic.ContainsKey(nums[i]))
-						_value_dic.Add(nums[i], value: i);
-				}
-			}
-			return returnValue;
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sum of two numbers
+            int[] nums = new int[] { 2, 11, 11, 7 };
+            int target = 9;
+            var algorithmEasy = new AlgorithmEasy();
+            Console.WriteLine("[" + string.Join(",", algorithmEasy.TwoSum(nums, target)) + "]");
+        }
+    }
 }
